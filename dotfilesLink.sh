aab20cd5940c1e1391afa2ce5a578ebc6f037196
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-05-16 23:45:51 +0900

    eslintへ切り替え

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,5 @@ ln -sf ~/Work/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Work/dotfiles/.zshrc ~/.zshrc 
 ln -sf ~/Work/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Work/dotfiles/.jshintrc ~/.jshintrc
+ln -sf ~/Work/dotfiles/.eslintrc ~/.eslintrc
 
