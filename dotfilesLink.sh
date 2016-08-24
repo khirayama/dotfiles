5bd3de4fbc721dfb46578b8b8ad17a996d39780d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-08-24 15:44:56 +0900

    Update dotfilesLink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -sf ~/Work/dotfiles/zshrc ~/.zshrc
+ln -sf $PWD/dotfiles/zshrc ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
-ln -sf ~/Work/dotfiles/vimrc ~/.vimrc
+ln -sf $PWD/dotfiles/vimrc ~/.vimrc
 ln -sf $PWD/ctags ~/.ctags
 ln -sf $PWD/gitconfig ~/.gitconfig
 ln -sf $PWD/eslintrc ~/.eslintrc
