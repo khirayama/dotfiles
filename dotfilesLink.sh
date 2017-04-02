87e92100d3d9f6475a4cca0e218340fea7ae8463
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-04-03 07:24:15 +0900

    Create xvimrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,7 @@
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
 ln -sf $PWD/vimrc ~/.vimrc
+ln -sf $PWD/xvimrc ~/.xvimrc
 ln -sf $PWD/ctags ~/.ctags
 ln -sf $PWD/gitconfig ~/.gitconfig
 ln -sf $PWD/eslintrc ~/.eslintrc
