8ab839159e477bcf14cd5be05dfec31d18c5c87f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-06-15 07:01:10 +0900

    Remove needless files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 mkdir -p ~/.vim/colors
 ln -sf $PWD/zshrc ~/.zshrc
-ln -sf $PWD/screenrc ~/.screenrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
-ln -sf $PWD/eslintrc ~/.eslintrc
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/original.vim ~/.vim/colors/original.vim
 ln -sf $PWD/gitconfig ~/.gitconfig
-ln -sf $PWD/gitconfig ~/.gitconfig
-sudo ln -snf $PWD/vscode/User $HOME/Library/Application\ Support/Code
