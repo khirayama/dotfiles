d757aa6a651497195f5c65762816643de80891af
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-06-16 06:58:04 +0900

    Create eslintrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,4 @@ ln -sf $PWD/tmux.conf ~/.tmux.conf
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/original.vim ~/.vim/colors/original.vim
 ln -sf $PWD/gitconfig ~/.gitconfig
+ln -sf $PWD/eslintrc ~/.eslintrc
