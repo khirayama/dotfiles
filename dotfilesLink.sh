35811bbbd42a7deada29a8c769c1bad8a64b7a4a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-12-01 17:08:24 +0900

    Update dotfilesLink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
+
+# Install HomeBrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew packages
+brew bunlde
+
+# Install dein(https://github.com/Shougo/dein.vim)
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+
+mkdir -p ~/.config/nvim
 mkdir -p ~/.vim/colors
+
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/original.vim ~/.vim/colors/original.vim
+ln -sf $PWD/init.vim ~/.config/nvim/init.vim
 ln -sf $PWD/gitconfig ~/.gitconfig
 ln -sf $PWD/eslintrc ~/.eslintrc
