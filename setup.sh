86cc9e298966c558bfffce1423c5b09da2447a23
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-12-13 07:34:55 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -16,8 +16,7 @@ mkdir -p ~/.vim/colors
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
 ln -sf $PWD/vimrc ~/.vimrc
-ln -sf $PWD/original.vim ~/.vim/colors/original.vim
-ln -sf $PWD/init.vim ~/.config/nvim/init.vim
+ln -sf $PWD/vimrc ~/.config/nvim/init.vim
 ln -sf $PWD/gitconfig ~/.gitconfig
 ln -sf $PWD/eslintrc ~/.eslintrc
 
