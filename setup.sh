0b4a253946bdde2e4e8b47879ba8ca08d9027f51
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-07-25 09:22:15 +0900

    Update setup.sh for Mac

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Install HomeBrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install brew packages
-brew bundle
-
 # Setup vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
