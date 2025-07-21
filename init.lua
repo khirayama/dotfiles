33edcbb916533a6f2e283ee3748e8b6c92a2113d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-07-21 21:19:56 +0900

    Add CopilotChat

--- init.lua
+++ init.lua
@@ -47,6 +47,18 @@ require("lazy").setup({
       { "<C-a>d", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 })
 
 -- 基本設定
