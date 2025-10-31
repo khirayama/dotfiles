1683627aab52f0313ad43976637173507e326013
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-10-31 22:31:01 +0900

    Update setup.ubuntu.sh

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 sudo apt update
-sudo apt install -y zsh
+sudo apt install -y zsh wslu language-pack-ja locales vim-gtk3 openjdk-21-jdk
+sudo locale-gen ja_JP.UTF-8
+sudo update-locale LANG=ja_JP.UTF-8 LC_ALL=ja_JP.UTF-8
 chsh -s $(which zsh)
 
 mkdir -p ~/.config/nvim
 mkdir -p ~/.vim/colors
 mkdir -p ~/.github/copilot
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # Setup vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -14,7 +19,7 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 ln -sf $PWD/zshrc.ubuntu ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
-ln -sf $PWD/vimrc ~/.vimrc
+ln -sf $PWD/vimrc.ubuntu ~/.vimrc
 ln -sf $PWD/init.lua ~/.config/nvim/init.lua
 ln -sf $PWD/gitconfig ~/.gitconfig
 ln -sf $PWD/eslintrc ~/.eslintrc
