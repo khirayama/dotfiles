114ce43caa73e1080c2225317816a4d38f40a909
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2026-03-17 06:49:37 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-mkdir -p ~/.config/nvim
 mkdir -p ~/.vim/colors
 mkdir -p ~/.github/copilot
 
@@ -12,6 +11,4 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
 ln -sf $PWD/vimrc ~/.vimrc
-ln -sf $PWD/init.lua ~/.config/nvim/init.lua
 ln -sf $PWD/gitconfig ~/.gitconfig
-ln -sf $PWD/eslintrc ~/.eslintrc
