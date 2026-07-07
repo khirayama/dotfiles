2fd3b92b0ba5118351340349c6da5b8fdca61c3f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2026-07-08 06:37:08 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
 mkdir -p ~/.vim/colors
-mkdir -p ~/.github/copilot
-mkdir -p ~/.config/ghostty
 
 # Setup vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Setup LazyVim for nvim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 ln -sf $PWD/zshrc ~/.zshrc
-ln -sf $PWD/tmux.conf ~/.tmux.conf
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/gitconfig ~/.gitconfig
-ln -sf $PWD/ghostty/config ~/.config/ghostty/config
